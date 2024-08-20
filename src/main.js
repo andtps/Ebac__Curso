c40ce351a12c905a645f5985b611dfabc08986fb
AndreItaSat <tpsandre19@gmail.com>
2024-08-20 10:02:52 -0300

    arquivos adicionados a branch

---
+++ src/main.js
@@ -0,0 +1,9 @@
+"use strict";
+function soma(a, b) {
+    return a * b;
+}
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+console.log(soma(3, 5));
+console.log(saudacao("Roberto"));
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+function soma(a:number, b:number):number{
+    return a * b
+}
+
+function saudacao(nome: string): string{
+    return "Olá " + nome;
+}
+
+console.log(soma(3,5))
+console.log(saudacao("Roberto"))
